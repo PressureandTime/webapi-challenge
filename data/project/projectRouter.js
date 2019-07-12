0340766a61ea87b9420d90f9c7a56b1d7f840370
Petar Scepanovic <hiborija@gmail.com>
2019-07-12 10:24:28 +0200

    added endpoint for updating, deleting the project

--- data/project/projectRouter.js
+++ data/project/projectRouter.js
@@ -1,13 +1,12 @@
 const express = require('express');
 
-const postDb = require('../helpers/projectModel');
+const projectDb = require('../helpers/projectModel');
 
 const router = express.Router();
 
-
 router.get('/', async (req, res) => {
   try {
-    const projects = await postDb.get();
+    const projects = await projectDb.get();
     res.status(200).json(projects);
   } catch (error) {
     console.log(error);
@@ -18,18 +17,60 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    try {
-      const project = await postDb.get(req.params.id);
-      res.status(200).json(project);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the project with that ID'
-      });
+  try {
+    const project = await projectDb.get(req.params.id);
+    res.status(200).json(project);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the project with that ID'
+    });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const project = await projectDb.insert(req.body);
+    res.status(201).json(project);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding the project'
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const updatedProject = await projectDb.update(id, req.body);
+    if (updatedProject) {
+      res.status(200).json({ message: 'Update was successful' });
+    } else {
+      res.status(404).json({ message: 'The project could not be found' });
     }
-  });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the project'
+    });
+  }
+});
 
-
-
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await projectDb.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'The project has been deleted' });
+    } else {
+      res.status(404).json({ message: 'The project could not be found' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the project'
+    });
+  }
+});
 
 module.exports = router;
