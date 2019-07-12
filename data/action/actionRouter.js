2ce9afb5405c320e4febb7c49a5ede65c5246390
Petar Scepanovic <hiborija@gmail.com>
2019-07-12 09:58:33 +0200

    added endpoint for fetching action by id, creating a new action and updating the action by id

--- data/action/actionRouter.js
+++ data/action/actionRouter.js
@@ -4,7 +4,6 @@ const actionDb = require('../helpers/actionModel');
 
 const router = express.Router();
 
-
 router.get('/', async (req, res) => {
   try {
     const actions = await actionDb.get();
@@ -17,6 +16,46 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const actions = await actionDb.get(req.params.id);
+    res.status(200).json(actions);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the action with that ID'
+    });
+  }
+});
 
+router.post('/', async (req, res) => {
+  try {
+    const action = await actionDb.insert(req.body);
+    res.status(201).json(action);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding the action'
+    });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const updatedAction = await actionDb.update(id, req.body);
+    if (updatedAction) {
+      res.status(200).json({ message: 'update was successful' });
+    } else {
+      res.status(404).json({ message: 'The action could not be found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the action'
+    });
+  }
+});
 
 module.exports = router;
